27c2c9e26578f850f4bad1a4f8f7ef56ddce2619
Uday Sai <udaysai7599@gmail.com>
2026-06-10 00:51:15 +0530

    initial commit

---
+++ blogSite/blogapp/src/components/ArticleList.jsx
@@ -0,0 +1,19 @@
+import Article from "./Article";
+
+// ArticleList component displays all blog posts
+function ArticleList({ posts }) {
+  return (
+    <main>
+      {posts.map((post) => (
+        <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+        />
+      ))}
+    </main>
+  );
+}
+//this is updated code
+export default ArticleList;
